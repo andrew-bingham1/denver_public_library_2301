5eafc3bd1b43ec4c25931e21a4cc9bfd8bad9f6c
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-24 09:42:32 -0700

    feat: add write method

--- lib/author.rb
+++ lib/author.rb
@@ -8,5 +8,18 @@ class Author
 
   end
 
-  def
+  def write(book_title, book_date)
+   @books << Book.new({
+      author_first_name: (@name.split(' ').first),
+      author_last_name: (@name.split(' ').last),
+      title: book_title,
+      publication_date: book_date
+      })
+
+    @books.last
+    
+
+  end
 end
+
+
