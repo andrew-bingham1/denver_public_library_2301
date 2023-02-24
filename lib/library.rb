fbc96f380020de37eb49a27e4ceae849fc2e7f2b
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-24 10:28:02 -0700

    feat: publication_time_frame_for method

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,18 @@ class Library
     end
     @authors << author
   end
+
+  def publication_time_frame_for(author)
+    publication_hash = {} 
+    books_arr = []
+      author.books.each do |book|
+      books_arr << book.publication_year.to_i
+      end
+    sorted_books_arr = books_arr.sort
+    publication_hash[:start] = sorted_books_arr.first.to_s
+    publication_hash[:end] = sorted_books_arr.last.to_s
+    publication_hash
+  end
+  
 end
+
