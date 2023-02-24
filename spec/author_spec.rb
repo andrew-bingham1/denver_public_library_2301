1fa6f7f9c89f418a7ba7250cdfb21c9c24348ea1
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-24 09:17:57 -0700

    test: create init test for author_spec

---
+++ spec/author_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do 
+  describe '#initialize' do
+    it 'can initialize' do 
+      charlotte_bronte = Author.new({
+        first_name: "Charlotte",
+        last_name: "Bronte"
+        })
+
+        expect(charlotte_bronte).to be_a Author
+        expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+        expect(charlotte_bronte.books).to eq([])
+    end
+  end
+end
