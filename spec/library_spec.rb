8315493084c33a18e4e8acb363cd2f0f6aed0baf
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-24 10:07:28 -0700

    test: add second add_author test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -33,5 +33,23 @@ RSpec.describe Library do
       expect(library.authors.first).to eq (charlotte_bronte)
     end
 
+    it 'can authors with their books2' do 
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({
+        first_name: "Charlotte",
+        last_name: "Bronte"
+        })
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+      harper_lee = Author.new({
+        first_name: "Harper",
+        last_name: "Lee"})
+      to_kill_a_mocking_bird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+      library.add_author(charlotte_bronte)
+      library.add_author(harper_lee)
+
+      expect(library.books.last.title).to eq("To Kill a Mockingbird")
+    end
   end
 end
